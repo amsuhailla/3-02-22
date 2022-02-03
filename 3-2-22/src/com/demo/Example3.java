641db4b952ecbb88c67b75631c034e0855f38be7
welcome <welcome@DESKTOP-AQKP6AL>
2022-02-03 16:30:28 +0530

    first commit

---
+++ 3-2-22/src/com/demo/Example3.java
@@ -0,0 +1,17 @@
+package com.demo;
+
+public class Example3 {
+
+	public static void main(String[] args) {
+		
+		int a=3;
+		
+		    int b=5;
+		    
+		        int c=a+b;
+		        
+		System.out.println(c);
+
+	}
+
+}
